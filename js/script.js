01a39334836a85727e35147c50d1fbce57ef9577
Ayme027 <aymejuliangut@gmail.com>
2024-11-02 00:07:02 +0100

    page-about-terminado

--- js/script.js
+++ js/script.js
@@ -56,3 +56,34 @@ function showDiv2(targetId) {
         }
     });
   }
+
+
+// CARD - TEAM
+$(document).ready(function () {
+    var swiper = new Swiper(".slide-content", {
+        slidesPerView: 3,
+        spaceBetween: 25,
+        loop: true,
+        fade: "true",
+        grabCursor: "true",
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          dynamicBullets: true,
+        },
+
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+
+            760: {
+                slidesPerView: 2,
+            },
+
+            950: {
+                slidesPerView: 3,
+            }
+        }
+    });
+});
